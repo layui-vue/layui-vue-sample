618e1a47f6da62b567f2caea8fed289e422d31eb
就眠仪式 <854085467@qq.com>
2021-09-30 02:42:26 +0800

    test 0.0.5

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -3,9 +3,16 @@
   <lay-header>
     <lay-logo></lay-logo>
   </lay-header>
-  <lay-side></lay-side>
+  <lay-side>
+    <lay-menu>
+      <lay-menu-item title="控制台"></lay-menu-item>
+      <lay-menu-item title="分析页"></lay-menu-item>
+    </lay-menu>
+  </lay-side>
   <lay-body>
-    <router-view></router-view>
+    <div style="padding:10px">
+      <router-view></router-view>
+    </div>
   </lay-body>
   <lay-footer></lay-footer>
 </lay-layout>
--- src/views/Console/index.vue
+++ src/views/Console/index.vue
@@ -1,3 +1,27 @@
 <template>
-    Console
+  <div>
+    <lay-button-container>
+      <lay-button>默认按钮</lay-button>
+      <lay-button>重要按钮</lay-button>
+    </lay-button-container>
+    <lay-form>
+      <lay-radio label="1" v-model="selected">写作</lay-radio>
+      <lay-radio label="2" v-model="selected">画画</lay-radio>
+      <lay-radio label="3" v-model="selected">运动</lay-radio>
+    </lay-form>
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+
+    const selected = ref("1");
+
+    return {
+      selected,
+    };
+  },
+});
+</script>
