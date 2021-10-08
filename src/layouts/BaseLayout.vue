fcfc9253dab16d4848e4ba0ef702c02db465b0ba
就眠仪式 <854085467@qq.com>
2021-10-08 17:50:50 +0800

    [升级] layui-vue 0.0.14

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,19 +1,25 @@
 <template>
-<lay-layout>
-  <lay-header>
-    <lay-logo></lay-logo>
-  </lay-header>
-  <lay-side black>
-    <lay-menu tree>
-      <lay-menu-item title="控制台"></lay-menu-item>
-      <lay-menu-item title="分析页"></lay-menu-item>
-    </lay-menu>
-  </lay-side>
-  <lay-body>
-    <div style="padding:10px">
-      <router-view></router-view>
-    </div>
-  </lay-body>
-  <lay-footer></lay-footer>
-</lay-layout>
+  <lay-layout>
+    <lay-header>
+      <lay-logo></lay-logo>
+    </lay-header>
+    <lay-side black>
+      <lay-menu selectedKey="5" tree>
+        <lay-menu-item title="首页" id="1"></lay-menu-item>
+        <lay-menu-item title="用户" id="2"></lay-menu-item>
+        <lay-menu-item title="角色" id="3"></lay-menu-item>
+        <lay-menu-item title="目录">
+          <lay-menu-child-item title="菜单一" id="4"></lay-menu-child-item>
+          <lay-menu-child-item title="菜单二" id="5"></lay-menu-child-item>
+          <lay-menu-child-item title="菜单三" id="6"></lay-menu-child-item>
+        </lay-menu-item>
+      </lay-menu>
+    </lay-side>
+    <lay-body>
+      <div style="padding: 10px">
+        <router-view></router-view>
+      </div>
+    </lay-body>
+    <lay-footer></lay-footer>
+  </lay-layout>
 </template>
--- src/views/Console/index.vue
+++ src/views/Console/index.vue
@@ -4,11 +4,6 @@
       <lay-button>默认按钮</lay-button>
       <lay-button>重要按钮</lay-button>
     </lay-button-container>
-    <lay-form>
-      <lay-radio label="1" v-model="selected">写作</lay-radio>
-      <lay-radio label="2" v-model="selected">画画</lay-radio>
-      <lay-radio label="3" v-model="selected">运动</lay-radio>
-    </lay-form>
   </div>
 </template>
 <script lang="ts">
