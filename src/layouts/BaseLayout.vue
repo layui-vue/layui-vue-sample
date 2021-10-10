8a72eb14e060a09a8b3c3db51c092766a7c3a4fc
就眠仪式 <854085467@qq.com>
2021-10-11 02:21:52 +0800

    升级 layui - vue 0.0.16 版本

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,12 +1,18 @@
 <template>
   <lay-layout>
     <lay-header>
-      <lay-logo>Layui - Admin</lay-logo>
+      <lay-logo class="layui-bg-black">Layui - Sample</lay-logo>
     </lay-header>
     <lay-side black>
-      <lay-menu selectedKey="1" tree>
-        <lay-menu-item title="控制台" id="1"></lay-menu-item>
-        <lay-menu-item title="分析页" id="2"></lay-menu-item>
+      <lay-menu selectedKey="5" tree>
+        <lay-menu-item title="首页" id="1"></lay-menu-item>
+        <lay-menu-item title="用户" id="2"></lay-menu-item>
+        <lay-menu-item title="角色" id="3"></lay-menu-item>
+        <lay-menu-item title="目录">
+          <lay-menu-child-item title="菜单一" id="4"></lay-menu-child-item>
+          <lay-menu-child-item title="菜单二" id="5"></lay-menu-child-item>
+          <lay-menu-child-item title="菜单三" id="6"></lay-menu-child-item>
+        </lay-menu-item>
       </lay-menu>
     </lay-side>
     <lay-body>
