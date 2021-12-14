5c517359ac6fab17da300de1546d010a01958697
就眠儀式 <854085467@qq.com>
2021-12-14 15:53:22 +0800

    升级 layui-vue 0.2.7

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script lang="ts" setup>
+// @ts-ignore
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <router-view></router-view>
+  <HelloWorld msg="layui-vue-sample" />
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import Layui from '@layui/layui-vue'
-import '@layui/layui-vue/lib/layui.css'
+import '@layui/layui-vue/lib/index.css'
+import '@layui/icons-vue/lib/index.css'
 
 import App from './App.vue'
-import Router from './router'
 
 createApp(App)
 .use(Layui)
-.use(Router)
 .mount('#app')
